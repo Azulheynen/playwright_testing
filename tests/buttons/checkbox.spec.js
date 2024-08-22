2ca9b381d6e2334978636165f1a03b3809b8f77b
Azul Heynen <azul@Azuls-MacBook-Pro.local>
2024-08-22 11:35:06 -0300

    incremental checkboxes function

--- tests/buttons/checkbox.spec.js
+++ tests/buttons/checkbox.spec.js
@@ -8,43 +8,27 @@ test("navigation", async ({ page }) => {
 test("mark single checkboxes", async ({ page }) => {
   await page.goto("https://qa-automation-practice.netlify.app/checkboxes");
 
-  selectors.setTestIdAttribute("id");
-
-  //finds checkbox, checks it, then reset the state
-  const checkbox1 = page.getByTestId("checkbox1");
-  await checkbox1.check();
-  await expect(checkbox1).toBeChecked();
   const reset = page.getByRole("button", { name: "Reset" });
-  await reset.click();
+  const checkboxes = await page.locator('input[type="checkbox"]');
+  const count = await checkboxes.count();
 
-  const checkbox2 = page.getByTestId("checkbox2");
-  await checkbox2.check();
-  await expect(checkbox2).toBeChecked();
-  await reset.click();
-
-  const checkbox3 = page.getByTestId("checkbox3");
-  await checkbox3.check();
-  await expect(checkbox3).toBeChecked();
-  await reset.click();
+  for (let i = 1; i < count; i++) {
+    const singleCheckbox = checkboxes.nth(i);
+    await singleCheckbox.check();
+    await expect(singleCheckbox).toBeChecked();
+    await reset.click();
+  }
 });
 
 test("mark multiple checkboxes", async ({ page }) => {
   await page.goto("https://qa-automation-practice.netlify.app/checkboxes");
 
-  selectors.setTestIdAttribute("id");
-  const checkbox1 = page.getByTestId("checkbox1");
-  await checkbox1.check();
+  const checkboxes = await page.locator('input[type="checkbox"]');
+  const count = await checkboxes.count();
 
-  const checkbox2 = page.getByTestId("checkbox2");
-  await checkbox2.check();
-
-  const checkbox3 = page.getByTestId("checkbox3");
-  await checkbox3.check();
-
-  //verifies if all checkboxes are in fact, checked
-  await Promise.all([
-    expect(checkbox2).toBeChecked(),
-    expect(checkbox3).toBeChecked(),
-    expect(checkbox1).toBeChecked(),
-  ]);
+  for (let i = 1; i < count; i++) {
+    const singleCheckbox = checkboxes.nth(i);
+    await singleCheckbox.check();
+    await expect(singleCheckbox).toBeChecked();
+  }
 });
